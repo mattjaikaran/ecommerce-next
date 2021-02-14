3610e693ea879ae4c3f2e536d5a11013954e1deb
Dylan Rowe <dylanthk@gmail.com>
2021-02-14 12:53:24 -0600

    register skeleton added

--- components/Register/index.jsx
+++ components/Register/index.jsx
@@ -1,20 +1,46 @@
 import './Register.module.scss'
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Form, Button, Row, Col, Container } from 'react-bootstrap';
 
 const Register = () => {
-  // bring in react hook form / build out the form
-  // bring in react bootstrap
 
-  // register form fields 
-  // email
-  // password
-  // password confirmation
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = data => console.log(data);
+  console.log(errors);
 
-  // 3 btn social auth (google, apple, fb)
-  // submit btn 
-  
-  return (
-    <div>Register</div>
-  )
+  return <>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Form.Group controlId="registerEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Email" name="Email" ref={register({ required: true, pattern: /^\S+@\S+$/i })} />
+      </Form.Group>
+      <Form.Group controlId="registerPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" name="Passworcd" ref={register} />
+      </Form.Group>
+      <Form.Group controlId="confirmPassword">
+        <Form.Label>Password Confirmation</Form.Label>
+        <Form.Control type="password" placeholder="Password Confirmation" name="Password Confirmation" ref={register({ max: 0 })} />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+        </Button>
+      <Container  className='button_Container mt-3'>
+        <Row>
+          <Button variant="primary" size="md" className="ml-3">
+            FaceBook
+    </Button>{' '}
+          <Button variant="primary" size="md" className="ml-3">
+            Twitter
+    </Button>{' '}
+          <Button variant="primary" size="md" className="ml-3">
+            Whatever
+    </Button>{' '}
+        </Row>
+      </Container>
+    </form>
+  </>
 }
 
 export default Register
