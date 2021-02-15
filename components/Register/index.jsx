61ff6dd212d51b36468db41abd5b72c2cbac5eb4
Dylan Rowe <dylanthk@gmail.com>
2021-02-15 13:20:31 -0600

    got the password error toggle working

--- components/Register/index.jsx
+++ components/Register/index.jsx
@@ -1,32 +1,40 @@
 import './Register.module.scss'
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { Form, Button, Row, Container } from 'react-bootstrap';
+import { Form, Button, Row, Container, Alert } from 'react-bootstrap';
+
 
 const Register = () => {
 
   const { register, handleSubmit, errors } = useForm();
-  const onSubmit = data => console.log(data);
-  console.log(errors);
+  const [pwError, setPwError] = useState('')
+
+  const onSubmit = (data) => {
+    console.log(data)
+    setPwError(data)
+  }
 
   return <>
     <form onSubmit={handleSubmit(onSubmit)}>
       <Form.Group controlId="registerEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Email" name="Email" ref={register({ required: true, pattern: /^\S+@\S+$/i })} />
+        <Form.Control type="email" placeholder="email" name="Email"
+          ref={register({ required: true, pattern: /^\S+@\S+$/i })} />
       </Form.Group>
       <Form.Group controlId="registerPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" name="Passworcd" ref={register} />
+        <Form.Control type="password" placeholder="password"
+          name="password" ref={register} />
       </Form.Group>
-      <Form.Group controlId="confirmPassword">
+      <Form.Group controlId="confirmpassword">
         <Form.Label>Password Confirmation</Form.Label>
-        <Form.Control type="password" placeholder="Password Confirmation" name="Password Confirmation" ref={register({ max: 0 })} />
+        <Form.Control type="password" placeholder="password confirmation"
+          name="passwordconfirmation" ref={register({ max: 0 })} />
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
         </Button>
-      <Container  className='button_Container mt-3'>
+      <Container className='button_Container mt-3'>
         <Row>
           <Button variant="primary" size="md" className="ml-3">
             FaceBook
@@ -35,9 +43,11 @@ const Register = () => {
             Twitter
     </Button>{' '}
           <Button variant="primary" size="md" className="ml-3">
-            Whatever
+            Apple
     </Button>{' '}
         </Row>
+        {pwError.password !== pwError.passwordconfirmation ? 
+        <Alert className="mt-3" variant={'warning'}>passwords do not match</Alert> : ''}
       </Container>
     </form>
   </>
