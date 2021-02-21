d31ec75ad6ab1de4fe7145fce95364578d2a40e7
mattjaikaran <mattjaikaran@gmail.com>
2021-02-21 17:15:22 -0500

    updated category img placeholder colors

--- components/Categories/index.jsx
+++ components/Categories/index.jsx
@@ -9,21 +9,21 @@ const Categories = () => {
         <Col md={9}>
           <Row>
             <Col className="mb-3" xs={12} md={7}>
-              <Category img="https://dummyimage.com/650x390/000/fff" />
+              <Category img="https://dummyimage.com/650x390/f1d1d0/000" />
             </Col>
             <Col className="mb-3" xs={6} md={5}>
-              <Category img="https://dummyimage.com/350x300/000/fff" />
+              <Category img="https://dummyimage.com/350x300/c7ffd8/000" />
             </Col>
             <Col className="mb-3" xs={6} md={5}>
-              <Category img="https://dummyimage.com/350x300/000/fff" />
+              <Category img="https://dummyimage.com/350x300/6ddccf/000" />
             </Col>
             <Col className="mb-3" md={7}>
-              <Category img="https://dummyimage.com/650x390/000/fff" />
+              <Category img="https://dummyimage.com/650x390/a7c5eb/000" />
             </Col>
           </Row>
         </Col>
         <Col className="mb-3" md={3}>
-          <Category img="https://dummyimage.com/450x1020/000/fff" />
+          <Category img="https://dummyimage.com/450x1020/ffefa1/000" />
         </Col>
       </Row>
     </div>
