7720b8e678dbade1781d04985d015cdbb95ff377
mattjaikaran <mattjaikaran@gmail.com>
2021-03-03 18:42:50 -0500

    updated product card btns in all screens and slider arrows in CategorySlider in all screens

--- components/ProductCard/index.jsx
+++ components/ProductCard/index.jsx
@@ -43,41 +43,55 @@ const ProductCard = (props) => {
 
   return (
     <div className={`${styles.productCardContainer}`}>
-      <Row>
-        {renderBadge()}
-      </Row>
-      <Card onClick={handleShow} className={`${styles.productCard} ${!sale && !newProduct ? 'mt-3' : ''}`}>
-        <Card.Img variant="top" src="https://dummyimage.com/275x360/ccc/000" fluid="true" />
-        <Card.Body className={`${styles.cardBody} d-flex flex-column pt-2 px-0`}>
+      <Row>{renderBadge()}</Row>
+      <Card
+        onClick={handleShow}
+        className={`${styles.productCard} ${
+          !sale && !newProduct ? "mt-3" : ""
+        }`}>
+        <Card.Img
+          variant="top"
+          src="https://dummyimage.com/275x360/ccc/000"
+          fluid="true" />
+        <Card.Body
+          className={`${styles.cardBody} d-flex flex-column pt-2 px-0`}>
           <Card.Title className={`${styles.cardTitle} text-muted`}>
             {title}
           </Card.Title>
           <Card.Subtitle className={`${styles.cardSubtitle}`}>
-            <span className={`${sale && 'text-danger'}`}>${sale ? sale.salePrice.toFixed(2) : price.toFixed(2)}</span>
-            {sale && <small className={styles.salePrice}>${price.toFixed(2)}</small>}
+            <span className={`${sale && "text-danger"}`}>
+              ${sale ? sale.salePrice.toFixed(2) : price.toFixed(2)}
+            </span>
+            {sale && (
+              <small className={styles.salePrice}>${price.toFixed(2)}</small>
+            )}
           </Card.Subtitle>
         </Card.Body>
       </Card>
       <div className={styles.addToCartBtnWrapper}>
-        <Button 
-          variant="outline-secondary" 
-          className={styles.addToCartBtn} 
+        <Button
+          variant="outline-secondary"
+          className={styles.addToCartBtn}
           onClick={() => addToCart(props.product)}>
-          <span>Add To Cart</span>
-          {/* <Image src={ShoppingCart} className={styles.shoppingCartIcon} alt="cart" /> */}
+          <span className="">Add To Cart</span>
+          {/* <Image
+            src={ShoppingCart}
+            className={styles.shoppingCartIcon}
+            alt="cart"
+          /> */}
         </Button>
-         <Button className={styles.quickView} variant="primary" onClick={handleShow}>
-          Quick View
+        <Button
+          className={styles.quickView}
+          variant="primary"
+          onClick={handleShow}>
+          <span className="">Quick View</span>
           {/* <Image src={View} className={styles.shoppingCartIcon} alt="cart" /> */}
         </Button>
       </div>
-       {/* <Button className={`${styles.quickViewDesktop} d-xs-none`} variant="primary" onClick={handleShow}>
+      {/* <Button className={`${styles.quickViewDesktop} d-xs-none`} variant="primary" onClick={handleShow}>
         Quick View
       </Button> */}
-      <ProductModal 
-        show={show} 
-        product={props.product}
-        onHide={handleClose} />
+      <ProductModal show={show} product={props.product} onHide={handleClose} />
     </div>
   )
 }
