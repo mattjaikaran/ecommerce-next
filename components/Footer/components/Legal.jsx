3521d68fcc1f3ddb99ed69fcafe8fcf6e691d610
mattjaikaran <mattjaikaran@gmail.com>
2021-03-09 16:28:39 -0500

    updated footer styles. bg color WIP. updated header nav links too.

--- components/Footer/components/Legal.jsx
+++ components/Footer/components/Legal.jsx
@@ -3,10 +3,11 @@ import styles from "../Footer.module.scss"
 
 const Legal = () => {
   return (
-    <Container className={`bg-light text-center p-5 ${styles.legalContainer}`} fluid>
-      <p>
-        &copy; 2021 ShopSite, All Rights Reserved.
-      </p>
+    <Container
+      className={`bg-light text-center p-5 ${styles.legalContainer}`}
+      fluid>
+      <p className="px-5 mb-0">&copy; 2021 ShopSite</p>
+      <small>All Rights Reserved.</small>
     </Container>
   )
 }
--- components/Header/components/MainNav.jsx
+++ components/Header/components/MainNav.jsx
@@ -25,31 +25,49 @@ const MainNav = () => {
         <Navbar className={styles.navbarContainer} expand="lg">
           <Navbar.Brand href="/">Store</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mx-auto">
-              <Nav.Link className="mx-3" href="/">
+          <Navbar.Collapse
+            className={styles.navbarCollapse}
+            id="basic-navbar-nav">
+            <Nav className={`text-left mx-auto ${styles.navItems}`}>
+              <Nav.Link className={`${styles.navLink}`} href="/">
                 Home
               </Nav.Link>
-              <Nav.Link className="mx-3" href="/category/womens">
-                Womens
+              <Nav.Link
+                className={`${styles.navLink}`}
+                href="/category/bedroom">
+                Bedroom
               </Nav.Link>
-              <Nav.Link className="mx-3" href="/category/mens">
-                Mens
+              <Nav.Link
+                className={`${styles.navLink}`}
+                href="/category/living-room">
+                Living Room
               </Nav.Link>
-              <Nav.Link className="mx-3" href="/category/accessories">
-                Accessories
+              <Nav.Link
+                className={`${styles.navLink}`}
+                href="/category/dining-room">
+                Dining Room
               </Nav.Link>
-              {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
+              <Nav.Link
+                className={`${styles.navLink}`}
+                href="/category/kitchen">
+                Kitchen
+              </Nav.Link>
+              <NavDropdown
+                className={`${styles.navLink}`}
+                title="More"
+                id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
             </Nav>
             <Form onSubmit={handleSubmit} inline>
               <FormControl
